6ee2118dce37d1364b4b7a1fce1a5556a529b044
Rodrigo Sejas <rodsejas@gmail.com>
2022-05-24 11:28:49 +1000

    Equipment POST req and form added

--- server/routes/equipments/equipments.js
+++ server/routes/equipments/equipments.js
@@ -2,6 +2,7 @@ import express from "express";
 import { supabase } from "../../db/client.js";
 
 const router = express.Router();
+router.use(express.json());
 
 router.get("/equipments", async (req, res) => {
   let { data: equipments, error } = await supabase
@@ -10,12 +11,19 @@ router.get("/equipments", async (req, res) => {
   res.status(200).json(equipments);
 });
 
-router.get("/equipments/:id", async (req, res) => {
+router.get("/equipment/:id", async (req, res) => {
+  const id = req.params.id;
   let { data: equipments, error } = await supabase
     .from("equipments")
     .select("*")
-    .eq("id", "1");
+    .eq("id", id);
   res.status(200).json(equipments);
+});
+
+router.post("/equipments", async (req, res) => {
+  const body = req.body;
+  console.log(body);
+  const { data, error } = await supabase.from("equipments").insert([body]);
 });
 
 export default router;
